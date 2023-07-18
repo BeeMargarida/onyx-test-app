cdc6937e1ebc2d7c75e22a04b79e5787e19b9dd2
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-07-18 15:06:39 +0100

    feat: multi-tab simple e2e test

--- tests/simple.spec.ts
+++ tests/simple.spec.ts
@@ -17,3 +17,18 @@ test('logs in after clicking button', async ({page}) => {
   const logOutButton = page.getByText('Log Out');
   await expect(logOutButton).toBeTruthy();
 });
+
+test('multiple tabs', async ({page, context}) => {
+  const secondPage = await context.newPage();
+
+  await Promise.all([page.goto('/'), secondPage.goto('/')]);
+
+  const logInButton = page.getByText('Log In');
+  await logInButton.click();
+
+  const logOutButtonPage = page.getByText('Log Out');
+  await expect(logOutButtonPage).toBeTruthy();
+
+  const logOutButtonSecondPage = secondPage.getByText('Log Out');
+  await expect(logOutButtonSecondPage).toBeTruthy();
+});
