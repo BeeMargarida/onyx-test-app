026ebdfde9808834e027f60bc08e77a4ac5a0a68
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-07-21 09:23:35 +0100

    test: improve multiple tabs test to increase probability of it failing

--- tests/tabs.spec.ts
+++ tests/tabs.spec.ts
@@ -18,32 +18,43 @@ test.describe('multiple tabs', () => {
 
   test('fetch should stop after log out', async ({page, context}) => {
     const secondPage = await context.newPage();
+    const thirdPage = await context.newPage();
 
-    await Promise.all([page.goto('/'), secondPage.goto('/')]);
+    await Promise.all([
+      page.goto('/'),
+      secondPage.goto('/'),
+      thirdPage.goto('/'),
+    ]);
 
     let logInButton = page.getByText('Log In');
     await logInButton.click();
 
     const logOutButtonFirstPage = page.getByText('Log Out');
     const logOutButtonSecondPage = secondPage.getByText('Log Out');
+    const logOutButtonThirdPage = thirdPage.getByText('Log Out');
 
     await expect(logOutButtonFirstPage).toBeTruthy();
     await expect(logOutButtonSecondPage).toBeTruthy();
+    await expect(logOutButtonThirdPage).toBeTruthy();
 
     const fetchPokedexButtonFirstPage = page.getByText('Fetch Pokedex');
+    const fetchPokedexButtonThirdPage = thirdPage.getByText('Fetch Pokedex');
 
     await Promise.all([
       logOutButtonSecondPage.click(),
       fetchPokedexButtonFirstPage.click(),
+      fetchPokedexButtonThirdPage.click(),
     ]);
 
     logInButton = page.getByText('Log In');
     expect(logInButton).toBeTruthy();
 
-    await page.reload();
+    await Promise.all([page.reload(), thirdPage.reload()]);
 
     // This sometimes fails
-    const fetchPokedexData = page.getByLabel('data-pokedex');
-    expect(fetchPokedexData).toBeEmpty();
+    const fetchPokedexDataFirstPage = page.getByLabel('data-pokedex');
+    const fetchPokedexDataThirdPage = thirdPage.getByLabel('data-pokedex');
+    expect(fetchPokedexDataFirstPage).toBeEmpty();
+    expect(fetchPokedexDataThirdPage).toBeEmpty();
   });
 });
