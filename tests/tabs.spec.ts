29acc1673da0f8bf229875a062366fdda5d3d694
Ana Margarida Silva <ana.margarida.silva@callstack.com>
2023-08-02 17:54:57 +0100

    test: e2e for the prop ordering issue

--- tests/tabs.spec.ts
+++ tests/tabs.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test.describe('multiple tabs', () => {
-  test('logs out in all tabs', async ({page, context}) => {
+  test('logs in in all tabs', async ({page, context}) => {
     const secondPage = await context.newPage();
 
     await Promise.all([page.goto('/'), secondPage.goto('/')]);
@@ -57,5 +57,48 @@ test.describe('multiple tabs', () => {
     const fetchSpaceDataThirdPage = thirdPage.getByLabel('data-meteorites');
     expect(fetchSpaceDataFirstPage).toBeEmpty();
     expect(fetchSpaceDataThirdPage).toBeEmpty();
+  });
+
+  test('should update in the same order between tabs', async ({
+    page,
+    context,
+  }) => {
+    await page.goto('/');
+
+    // Logs in and fetches data
+    let logInButton = page.getByText('Log In');
+    await logInButton.click();
+
+    const fetchDataButton = page.getByText('Fetch Space data (small)');
+    await fetchDataButton.click();
+
+    // Opens new page and reloads the first one
+    const secondPage = await context.newPage();
+    await Promise.all([page.reload(), secondPage.goto('/')]);
+
+    // Checks that the update keys ordering is matching
+    let updatesFirstPage = page.getByLabel('data-updates');
+    let updatesSecondPage = secondPage.getByLabel('data-updates');
+
+    expect(updatesFirstPage.innerText).toEqual(updatesSecondPage.innerText);
+
+    const logOutButtonSecondPage = secondPage.getByText('Log Out');
+    await expect(logOutButtonSecondPage).toBeTruthy();
+    await logOutButtonSecondPage.click();
+
+    const logInButtonFirstPage = page.getByText('Log In');
+    const logInButtonSecondPage = secondPage.getByText('Log In');
+
+    await expect(logInButtonFirstPage).toBeTruthy();
+    await expect(logInButtonSecondPage).toBeTruthy();
+
+    updatesFirstPage = page.getByLabel('data-updates');
+    updatesSecondPage = secondPage.getByLabel('data-updates');
+
+    const updatesTextFirstPage = await updatesFirstPage.innerText();
+    const updatesTextSecondPage = await updatesSecondPage.innerText();
+
+    // This should fail
+    expect(updatesTextFirstPage).toEqual(updatesTextSecondPage);
   });
 });
